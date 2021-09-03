fb635ab3946d4b8ca90a106ada65bce3c618293e
Aliaksandr Bulynka <abulynka@gmail.com>
2021-09-03 15:57:48 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.current = Math.ceil(this.min + (this.max - this.min)/2);
     }
 
     guess() {
-
+        return this.current;
     }
 
     lower() {
-
+        this.max = this.current;
+        this.current = Math.ceil(this.min + (this.max - this.min)/2);
     }
 
     greater() {
-
+        this.min = this.current;
+        this.current = Math.ceil(this.min + (this.max - this.min)/2);
     }
 }
 
